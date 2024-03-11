28143038a1f1ad2f6fb6374b33ec2aeff0f94133
Usuario <Usuario@CPD03-M13>
2024-03-11 12:23:42 -0300

    Avanço no projeto.

---
+++ MauiAppListaCompras/Views/NovoProduto2.xaml.cs
@@ -0,0 +1,30 @@
+using MauiAppListaCompras.Models;
+
+namespace MauiAppListaCompras.Views;
+
+public partial class NovoProduto2 : ContentPage
+{
+	public NovoProduto2()
+	{
+		InitializeComponent();
+	}
+
+    private async void ToolbarItem_Clicked(object sender, EventArgs e)
+    {
+		try 
+		{ 
+			Produto p = new Produto();
+			{
+				Descricao = txt_descricao.Text,
+				Quantidade = Convert.ToDouble(txt_quantidade.Text),
+			    Preco = Convert.ToDouble(txt_preco.Text);	
+			};
+			 await App.Db.Insert(p);
+			 await DisplayAlert("Sucesso!", "Produto inserido", "OK"); 
+		} 
+		catch (Exception ex) 
+		{ 
+			await DisplayAlert("Ops", ex.Message, "Fechar");
+        }
+    }
+}
